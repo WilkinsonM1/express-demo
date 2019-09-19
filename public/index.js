d5038970ad40f5a1cf52710300b30725167ca4cd
CN72 <cn72@CN72.local>
2019-09-19 11:07:10 +0100

    added drumkit files to public to serve it and installed nodemon

--- public/index.js
+++ public/index.js
@@ -1,1 +1,65 @@
-console.log("hello from the back end")
+//all the audio tags stored in variables
+let clapSound = document.getElementById("clap-sound")
+let hihatSound = document.getElementById("hihat-sound")
+let kickSound = document.getElementById("kick-sound")
+let openhatSound = document.getElementById("openhat-sound")
+let boomSound = document.getElementById("boom-sound")
+let rideSound = document.getElementById("ride-sound")
+let snareSound = document.getElementById("snare-sound")
+let tomSound = document.getElementById("tom-sound")
+let tinkSound = document.getElementById("tink-sound")
+
+// Play drum by keyboard press
+
+window.addEventListener("keypress", () => {
+    if(event.key == "a"){ clapper() }
+    else if(event.key == "s"){ hihatter() }
+    else if(event.key == "d"){ kicker() }
+    else if(event.key == "f"){ openhatter() }
+    else if(event.key == "g"){ boomer() }
+    else if(event.key == "h"){ rider() }
+    else if(event.key == "j"){ snarer() }
+    else if(event.key == "k"){ tommer() }
+    else if(event.key == "l"){ tinker() }
+})
+
+
+
+// Play music by mouse click (each button calls the function onclick)
+//list of functions that play each sound file
+
+const clapper = () =>{
+    clapSound.play()
+}
+
+const hihatter = () =>{
+    hihatSound.play()
+}
+
+const kicker = () =>{
+    kickSound.play()
+}
+
+const openhatter = () =>{
+    openhatSound.play()
+}
+
+const boomer = () =>{
+    boomSound.play()
+}
+
+const rider = () =>{
+    rideSound.play()
+}
+
+const snarer = () =>{
+    snareSound.play()
+}
+
+const tommer = () =>{
+    tomSound.play()
+}
+
+const tinker = () =>{
+    tinkSound.play()
+}
